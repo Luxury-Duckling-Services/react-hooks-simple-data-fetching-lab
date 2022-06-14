80760fc65687fe75de4f4138ce60a82d5b14d8ca
Yifan <datasciencewyfan@gmail.com>
2022-06-14 10:41:42 -0700

    commit tree 0a470f7b055bc849cb8800ea692bb2e7bc11264a

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,24 @@
 // create your App component here
+import React , { useState , useEffect } from "react";
+
+function App() {
+    const [ link , setLink ] = useState('')
+    const [ loaded , setLoaded ] = useState(false)
+
+    useEffect(() => {
+        fetch('https://dog.ceo/api/breeds/image/random')
+        .then(resp=>resp.json())
+        .then(obj => {
+            setLink(obj.message)
+            setLoaded(true)
+        })
+    } , [])
+
+    if (! loaded) return <p>Loading...</p>
+
+    return (
+        <img src={link} alt='A Random Dog'></img>
+    )
+}
+
+export default App
